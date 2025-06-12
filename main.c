f5b26649d617488c1436e30cc419b13d255187ff
plinio <plinio@noreply.forge.a-lec.org>
2025-06-12 05:11:14 +0200

    aggiustamenti alla griglia

--- main.c
+++ main.c
@@ -26,7 +26,8 @@ typedef struct
 
 typedef struct
 {
-    double scale;
+    double scale_x;
+    double scale_y;
     double offset_x;
     double offset_y;
 
@@ -48,13 +49,15 @@ typedef struct
 
     int drawing_area_width;
     int drawing_area_height;
+    float grid_min_x;
+    float grid_max_x;
+    float grid_min_y;
+    float grid_max_y;
 
     AppWidgets widgets;
 
     telemetry *tel;
 } AppData;
-
-
 
 // Funzione per disegnare la griglia
 static void draw_grid(cairo_t *cr, AppData *data)
@@ -64,15 +67,15 @@ static void draw_grid(cairo_t *cr, AppData *data)
     cairo_set_line_width(cr, 0.5);
 
     // Disegna linee orizzontali
-    for (int i = -10; i <= 10; i++) {
-        cairo_move_to(cr, -(10 * data->scale) + data->offset_x, (i * data->scale) + data->offset_y);
-        cairo_line_to(cr, (10 * data->scale) + data->offset_x, (i * data->scale) + data->offset_y);
+    for (int i = data->grid_min_y; i <= data->grid_max_y; i++) {
+        cairo_move_to(cr, (data->grid_min_x * data->scale_x) + data->offset_x, (i * data->scale_y) + data->offset_y);
+        cairo_line_to(cr, (data->grid_max_x * data->scale_x) + data->offset_x, (i * data->scale_y) + data->offset_y);
     }
 
     // Disegna linee verticali
-    for (int i = -10; i <= 10; i++) {
-        cairo_move_to(cr, (i * data->scale) + data->offset_x, -(10 * data->scale) + data->offset_y);
-        cairo_line_to(cr, (i * data->scale) + data->offset_x, (10 * data->scale) + data->offset_y);
+    for (int i = data->grid_min_x; i <= data->grid_max_x; i++) {
+        cairo_move_to(cr, (i * data->scale_x) + data->offset_x, (data->grid_min_y * data->scale_y) + data->offset_y);
+        cairo_line_to(cr, (i * data->scale_x) + data->offset_x, (data->grid_max_y * data->scale_y) + data->offset_y);
     }
     cairo_stroke(cr);
 
@@ -80,8 +83,8 @@ static void draw_grid(cairo_t *cr, AppData *data)
     cairo_set_font_size(cr, 16);
 
     // Disegna il valore della coordinata verticale
-    for (int i = -10; i <= 10; i++) {
-        double y = (i * data->scale) + data->offset_y;
+    for (int i = data->grid_min_y; i <= data->grid_max_y; i++) {
+        double y = (i * data->scale_y) + data->offset_y;
 
         cairo_set_source_rgba(cr, 0.2, 0.8, 0.2, 0.75); // verde
         cairo_move_to(cr, 10, y + 5);
@@ -91,8 +94,8 @@ static void draw_grid(cairo_t *cr, AppData *data)
     int align = ALIGN_MIDDLE;
 
     // Disegna il valore della coordinata orizzontale
-    for (int i = -10; i <= 10; i++) {
-        double x = (i * data->scale) + data->offset_x;
+    for (int i = data->grid_min_x; i <= data->grid_max_x; i++) {
+        double x = (i * data->scale_x) + data->offset_x;
 
         char text[10];
         snprintf(text, sizeof(text), "%d", i);
@@ -123,15 +126,15 @@ static void draw_grid(cairo_t *cr, AppData *data)
     // asse orizzontale rosso
     cairo_set_source_rgb(cr, 0.8, 0.0, 0.0);
     cairo_set_line_width(cr, 2.0);
-    cairo_move_to(cr, -(10 * data->scale) + data->offset_x, (0 * data->scale) + data->offset_y);
-    cairo_line_to(cr, (10 * data->scale) + data->offset_x, (0 * data->scale) + data->offset_y);
+    cairo_move_to(cr, (data->grid_min_x * data->scale_x) + data->offset_x, (0 * data->scale_y) + data->offset_y);
+    cairo_line_to(cr, (data->grid_max_x * data->scale_x) + data->offset_x, (0 * data->scale_y) + data->offset_y);
     cairo_stroke(cr);
 
     // asse verticale verde
     cairo_set_source_rgb(cr, 0.0, 0.8, 0.0);
     cairo_set_line_width(cr, 2.0);
-    cairo_move_to(cr, (0 * data->scale) + data->offset_x, -(10 * data->scale) + data->offset_y);
-    cairo_line_to(cr, (0 * data->scale) + data->offset_x, (10 * data->scale) + data->offset_y);
+    cairo_move_to(cr, (0 * data->scale_x) + data->offset_x, (data->grid_min_y * data->scale_y) + data->offset_y);
+    cairo_line_to(cr, (0 * data->scale_x) + data->offset_x, (data->grid_max_y * data->scale_y) + data->offset_y);
     cairo_stroke(cr);
 }
 
@@ -148,20 +151,29 @@ void draw_graph(cairo_t *cr, AppData *data)
 
     telemetryData *td = data->tel->data;
 
-    float td0time = td[0].time;
-    float td0distance = (td[0].distance/50);
-    if (td0time > 0 && td0time < data->drawing_area_width &&
-        td0distance > 0 && td[0].distance < data->drawing_area_height) {
-        // Inizia a disegnare il grafico
-        cairo_move_to(cr, td0time * data->scale + data->offset_x, td0distance * data->scale + data->offset_y);
+    float td0time = td[1].time;
+    float td0distance = td[1].distance;
+    if (td0time > 0 &&
+        td0time < data->drawing_area_width &&
+        td0distance > 0 &&
+        td0distance < data->drawing_area_height)
+    {
+        cairo_move_to(cr,
+                (td0time * data->scale_x) + data->offset_x,
+                (td0distance * data->scale_y) + data->offset_y);
     }
 
-    for (size_t i = 1; i < data->tel->num_samples; i++) {
+    for (size_t i = 2; i < data->tel->num_samples; i++) {
         float tdItime = td[i].time;
-        float tdIdistance = (td[i].distance/50);
-        if (tdItime * data->scale + data->offset_x> 0 && tdItime * data->scale + data->offset_x< data->drawing_area_width &&
-            tdIdistance  * data->scale + data->offset_y> 0 && td[i].distance  * data->scale + data->offset_y< data->drawing_area_height) {
-            cairo_line_to(cr, tdItime * data->scale + data->offset_x, tdIdistance * data->scale + data->offset_y);
+        float tdIdistance = td[i].distance;
+        if (tdItime * data->scale_x + data->offset_x > 0 &&
+            tdItime * data->scale_x + data->offset_x < data->drawing_area_width &&
+            tdIdistance  * data->scale_y + data->offset_y > 0 &&
+            tdIdistance  * data->scale_y + data->offset_y < data->drawing_area_height)
+        {
+            cairo_line_to(cr,
+                    tdItime * data->scale_x + data->offset_x,
+                    tdIdistance * data->scale_y + data->offset_y);
         }
     }
     cairo_stroke(cr);
@@ -189,14 +201,6 @@ static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, AppData *data)
 
     cairo_set_source_rgb(cr, 1, 1, 1);
 
-//    cairo_move_to(cr, 10, 30);
-//    cairo_show_text(cr,
-//            (data->selecting ?
-//                    "selecting" :
-//                    data->panning ?
-//                            "panning" :
-//                            "")); // Scrivi il valore
-
     return TRUE; // Restituisce FALSE per permettere ulteriori elaborazioni
 }
 
@@ -220,10 +224,9 @@ static gboolean on_motion_notify_event(GtkWidget *widget, GdkEventMotion *event,
     data->mouse_pos_Y = event->y;
 
     gtk_label_set_text (GTK_LABEL(data->widgets.label),
-            g_strdup_printf("x:%+.3f\ty:%+.3f",
-                        (data->select_start_x * data->scale - data->offset_x + data->mouse_pos_X) / data->scale,
-                        (data->select_start_y * data->scale - data->offset_y + data->mouse_pos_Y) / data->scale));
-
+            g_strdup_printf("time:%.4f\tdistance:%.4f",
+                        (data->select_start_x * data->scale_x - data->offset_x + data->mouse_pos_X) / data->scale_x,
+                        (data->select_start_y * data->scale_y - data->offset_y + data->mouse_pos_Y) / data->scale_y));
 
     return TRUE;
 }
@@ -263,8 +266,8 @@ static gboolean on_button_press_event(GtkWidget *widget, GdkEventButton *event, 
     if (data->selecting && (event->button == GDK_BUTTON_PRIMARY)) {
         data->selecting = TRUE;
 
-        data->select_start_x = (data->scale - data->mouse_pos_X + data->offset_x) / data->scale;
-        data->select_start_y = (data->scale - data->mouse_pos_Y + data->offset_y) / data->scale;
+        data->select_start_x = (data->scale_x - data->mouse_pos_X + data->offset_x) / data->scale_x;
+        data->select_start_y = (data->scale_y - data->mouse_pos_Y + data->offset_y) / data->scale_y;
         data->select_start_x = event->x;
         data->select_start_y = event->y;
         data->select_end_x = event->x;
@@ -352,18 +355,20 @@ static gboolean on_scroll_event(GtkWidget *widget, GdkEventScroll *event, AppDat
     double mouse_y = event->y;
 
     // Calcola le coordinate della griglia prima dello zoom
-    double grid_x = (mouse_x - data->offset_x) / data->scale;
-    double grid_y = (mouse_y - data->offset_y) / data->scale;
+    double grid_x = (mouse_x - data->offset_x) / data->scale_x;
+    double grid_y = (mouse_y - data->offset_y) / data->scale_y;
 
     if (event->direction == GDK_SCROLL_UP) {
-        data->scale *= zoom_factor;
+        data->scale_x *= zoom_factor;
+        data->scale_y *= zoom_factor;
     } else if (event->direction == GDK_SCROLL_DOWN) {
-        data->scale /= zoom_factor;
+        data->scale_x /= zoom_factor;
+        data->scale_y /= zoom_factor;
     }
 
     // Calcola il nuovo offset in modo che il punto sotto il mouse rimanga fisso
-    data->offset_x = mouse_x - grid_x * data->scale;
-    data->offset_y = mouse_y - grid_y * data->scale;
+    data->offset_x = mouse_x - grid_x * data->scale_x;
+    data->offset_y = mouse_y - grid_y * data->scale_y;
 
     gtk_widget_queue_draw(widget); // Richiesta di ridisegno
     return TRUE;
@@ -371,6 +376,10 @@ static gboolean on_scroll_event(GtkWidget *widget, GdkEventScroll *event, AppDat
 
 int main(int argc, char *argv[])
 {
+//----------------------------------------------------------------------------------------
+// init telemetry
+//----------------------------------------------------------------------------------------
+
     if (argc < 4) {
         char buf[1024] = {};
         snprintf(buf, sizeof(buf), "Uso: %s <nome file csv> <numero lap> <stampa lap>", argv[0]);
@@ -427,29 +436,25 @@ int main(int argc, char *argv[])
 //        }
 //    }
 
-#if 0
-    gtk_init(&argc, &argv);
-
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Grafico con Cairo");
-    gtk_window_set_default_size(GTK_WINDOW(window), WIDTH, HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), tel);
-
-    GtkWidget *drawing_area = gtk_drawing_area_new();
-    gtk_container_add(GTK_CONTAINER(window), drawing_area);
-    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(on_draw_event), tel);
-
-    gtk_widget_show_all(window);
-    gtk_main();
-#endif
-
     closeFile(f);
+
 //----------------------------------------------------------------------------------------
+// init GTK
 //----------------------------------------------------------------------------------------
+
     AppData data = {}; // Inizializza i dati
     data.tel = tel;
     data.drawing_area_width = WIDTH;
     data.drawing_area_height = HEIGHT;
+    data.scale_x = 1.0;
+    data.scale_y = 0.01;
+    data.offset_x = data.drawing_area_width/2.0;
+    data.offset_y = data.drawing_area_height/2.0; // Inizializza i dati
+    int tel_num_samples = tel->num_samples;
+    data.grid_min_x = tel->data[1].time;
+    data.grid_max_x = tel->data[tel_num_samples-1].time;;
+    data.grid_min_y = tel->data[1].distance;
+    data.grid_max_y = tel->data[tel_num_samples-1].distance;;
 
     gtk_init(&argc, &argv);
 
@@ -469,10 +474,6 @@ int main(int argc, char *argv[])
 
     gtk_builder_connect_signals(builder, NULL);
 
-    data.scale = 15.0;
-    data.offset_x = data.drawing_area_width/2.0;
-    data.offset_y = data.drawing_area_height/2.0; // Inizializza i dati
-
     data.widgets.drawing_area = GTK_WIDGET(gtk_builder_get_object(builder, "drawing_area"));
     g_signal_connect(G_OBJECT(data.widgets.drawing_area), "draw", G_CALLBACK(on_draw_event), &data);
 
@@ -485,8 +486,13 @@ int main(int argc, char *argv[])
     g_signal_connect(G_OBJECT(data.widgets.drawing_area), "scroll-event", G_CALLBACK(on_scroll_event), &data);
 
     // Abilita gli eventi del mouse
-    gtk_widget_set_events(data.widgets.drawing_area, gtk_widget_get_events(data.widgets.drawing_area) |
-            GDK_BUTTON_PRESS_MASK | GDK_POINTER_MOTION_MASK | GDK_BUTTON_RELEASE_MASK | GDK_SCROLL_MASK);
+    gtk_widget_set_events(
+            data.widgets.drawing_area,
+            gtk_widget_get_events(data.widgets.drawing_area)
+                | GDK_BUTTON_PRESS_MASK
+                | GDK_POINTER_MOTION_MASK
+                | GDK_BUTTON_RELEASE_MASK
+                | GDK_SCROLL_MASK);
 
     gtk_widget_show_all(data.widgets.window);
     gtk_main();
